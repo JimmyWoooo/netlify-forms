3e885d5a6ed8a9a3fcd7c1f7697ed4341db8fa8d
JimmyWoooo <zhimingjimmy.wu@cix.csi.cuny.edu>
2022-10-30 23:00:59 -0400

    netlify forms

--- src/form.js
+++ src/form.js
@@ -27,21 +27,23 @@ const Form  = () =>{
 
     const handleChange = e => setState({ [e.target.name]: e.target.value });
 
+    const { name, email, message } = state;
+
     return(
       <form onSubmit={handleSubmit}>
       <p>
         <label>
-          Your Name: <input type="text" name="name" value={state.name} onChange={handleChange} />
+          Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
         </label>
       </p>
       <p>
         <label>
-          Your Email: <input type="email" name="email" value={state.email} onChange={handleChange} />
+          Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
         </label>
       </p>
       <p>
         <label>
-          Message: <textarea name="message" value={state.message} onChange={handleChange} />
+          Message: <textarea name="message" value={message} onChange={handleChange} />
         </label>
       </p>
       <p>
